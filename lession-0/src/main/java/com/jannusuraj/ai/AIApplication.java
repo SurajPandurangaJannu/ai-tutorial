8e8843bd84a7ad68d3cfb5954f793ec54040281d
Suraj Jannu <suraj.jannu@sap.com>
2024-10-07 20:47:29 +0530

    Initial Commit

---
+++ lession-0/src/main/java/com/jannusuraj/ai/AIApplication.java
@@ -0,0 +1,11 @@
+package com.jannusuraj.ai;
+
+import org.springframework.boot.SpringApplication;
+
+public class AIApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(AIApplication.class,args);
+    }
+}
